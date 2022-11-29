93a4f3852b5a364079ee88bae9dda4ab7dd52c5e
Tia Mehta <tmehta@wpi.edu>
2022-11-29 13:46:58 -0800

    did a lot

---
+++ server.js
@@ -0,0 +1,32 @@
+
+const { request, reponse } = require('express');
+
+const express = require('express'),
+    path = require('path')
+    port = 3000;
+
+const app = express();
+
+console.log('server is running');
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/index.html'));
+})
+
+app.get('/index.html', (req, res) => {
+    res.sendFile(path.join(__dirname + '/index.html'));
+})
+
+app.get('/style.css', (req, res) => {
+    res.sendFile(path.join(__dirname + '/style.css'))
+})
+
+app.get('/signup.html', (req, res) => {
+    res.sendFile(path.join(__dirname + '/signup.html'))
+})
+
+app.get('/login.html', (req, res) => {
+    res.sendFile(path.join(__dirname + '/login.html'))
+})
+
+app.listen(process.env.PORT || port);
